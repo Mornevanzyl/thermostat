106ce6aa3330196848ca1b306112d89304bc5872
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-02-12 10:12:57 +0000

    Added energy-usage unit tests and app functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,48 +7,90 @@ describe('Thermostat', () => {
     thermostat = new Thermostat();
   });
 
-  it('checks the starting temperature is 20', () => {
-    expect(thermostat.temperature).toEqual(20);
+  describe('Thermostat Controls: ', () => {
+    
+    it('The UP button ncreases temperature by 1', () => {
+      thermostat.up()
+      expect(thermostat.temperature).toEqual(21);
+    });
+  
+    it('The DOWN button decreases temperature by 1', () => {
+      thermostat.down()
+      expect(thermostat.temperature).toEqual(19);
+    });
+
+    it('The RESET button sets the temperature to 20', () => {
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+  });
+  
+  describe('Reports energy usage of ', () => {
+    
+    it('LOW when temperature is < 18', () => {
+      for (var i = 0; i < 3; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+
+    it('MEDIUM when temperature is <= 25', () => {
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+
+    it('HIGH when temperature is > 25', () => {
+      thermostat.setPowerSaveMode(false);
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+
   });
 
-  it('Increases temperature by 1', () => {
-    thermostat.up()
-    expect(thermostat.temperature).toEqual(21);
+  describe('Basic settings', () => {
+    
+    it('checks the starting temperature is 20', () => {
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+    it("temperature can't decrease lower than 10", () => {
+      for (var i = 0; i < 10; i++) {
+        thermostat.down();
+      }
+      expect(function() { thermostat.down() ;}).toThrow("Minimum temperature reached")
+    });
+    
   });
 
-  it('Decreases temperature by 1', () => {
-    thermostat.down()
-    expect(thermostat.temperature).toEqual(19);
-  });
 
-  it("temperature can't decrease lower than 10", () => {
-    for (var i = 0; i < 10; i++) {
-      thermostat.down();
-    }
-    expect(function() { thermostat.down() ;}).toThrow("Minimum temperature reached")
-  });
+  describe('PowerSave mode, ', () => {
 
-  describe('Powersave functionality', () => {
-
-    it("starts with power saving mode ON", () => {
+    it("starts with state = ON", () => {
       expect(thermostat.isPowerSaveOn()).toBe(true);
     });
 
-    it('has a max temperature of 25 when ON', () => {
+    it('allows a max temperature of 25 when ON', () => {
       for (var i = 0; i < 5; i++) {
         thermostat.up();
       }
       expect(function() { thermostat.up() ;}).toThrow("Maximum temperature for PowerSave mode reached")
     });
 
-    it('has a max temperature of 32 when OFF', () => {
-      thermostat.powerSaveOn = false;
+    it('allows a max temperature of 32 when OFF', () => {
+      thermostat.setPowerSaveMode(false);
       for (var i = 0; i < 12; i++) {
         thermostat.up();
       }
       expect(function() { thermostat.up() ;}).toThrow("Maximum temperature reached")
     });
-  });
+
+    it('can be turned OFF', () => {
+      thermostat.setPowerSaveMode(false);
+      expect(thermostat.powerSave).toBe(false);
+    });
+});
 
   
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,21 +3,21 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
-    this.powerSaveOn = true;
+    this.powerSave = true;
     this.POWERSAVE_ON_MAX = 25;
     this.POWERSAVE_OFF_MAX = 32;
   };
 
-  up(){
-    if (this.powerSaveOn && this.temperature === this.POWERSAVE_ON_MAX) {
+  up() {
+    if (this.isPowerSaveOn() && this.temperature >= this.POWERSAVE_ON_MAX) {
       throw("Maximum temperature for PowerSave mode reached");
-    } else if (this.temperature === this.POWERSAVE_OFF_MAX) {
+    } else if (this.temperature >= this.POWERSAVE_OFF_MAX) {
       throw("Maximum temperature reached");
     };
     this.temperature ++;
   };
 
-  down(){
+  down() {
     if (this.temperature > 10) {
       this.temperature --;
     } else {
@@ -25,7 +25,26 @@ class Thermostat {
     }
   };
 
+  reset() {
+    this.temperature = 20;
+  }
+
   isPowerSaveOn() {
-    return this.powerSaveOn;
+    return this.powerSave;
   };
+
+  setPowerSaveMode(mode) {
+    this.powerSave = mode;
+  }
+
+  energyUsage() {
+    console.log("hello")
+    if (this.temperature < 18) {
+      return 'low-usage';
+    } else if (this.temperature <= 25) {
+      return 'medium-usage';
+    };
+    console.log('final')
+    return 'high-usage';
+  }
 }
